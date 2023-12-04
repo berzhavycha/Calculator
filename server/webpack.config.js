a235413508953725ec9dff26305d9b6401ae270d
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2023-12-04 11:12:51 +0200

    add global folder for evironment variables

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -16,6 +16,7 @@ module.exports = {
             '@database': path.resolve(__dirname, 'src/database/index.ts'),
             '@modules': path.resolve(__dirname, 'src/modules/'),
             '@utils': path.resolve(__dirname, 'src/utils/'),
+            '@global': path.resolve(__dirname, 'src/global/'),
         }
     },
     module: {
