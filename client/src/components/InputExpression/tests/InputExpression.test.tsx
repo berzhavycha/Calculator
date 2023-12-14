09d1cd60a9699762fd0f8820b0aab654017c1b8a
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2023-12-14 12:48:28 +0200

    covered InputExpression with tests

---
+++ client/src/components/InputExpression/tests/InputExpression.test.tsx
@@ -0,0 +1,61 @@
+import { fireEvent, render, waitFor } from '@testing-library/react';
+import { InputExpression } from '../index';
+import { vi } from 'vitest';
+import userEvent from '@testing-library/user-event';
+
+describe('InputExpression component', () => {
+    const mockOnExpressionChange = vi.fn();
+    const mockOnEnter = vi.fn();
+
+    test('triggers onEnter function when Enter key is pressed', async () => {
+        const { getByRole } = render(
+            <InputExpression
+                expression=""
+                onExpressionChange={mockOnExpressionChange}
+                onEnter={mockOnEnter}
+            />
+        );
+
+        const inputElement = getByRole('textbox');
+        const inputExpression = '2+2'
+
+        fireEvent.change(inputElement, { target: { value: inputExpression } });
+        await userEvent.type(inputElement, '{enter}');
+
+        await waitFor(() => {
+            expect(mockOnExpressionChange).toHaveBeenCalledWith(inputExpression);
+            expect(mockOnEnter).toHaveBeenCalled();
+        });
+    });
+
+    test('should render with the provided expression value', () => {
+        const expression = '2 + 2';
+
+        const { getByDisplayValue } = render(
+            <InputExpression
+                expression={expression}
+                onExpressionChange={mockOnExpressionChange}
+                onEnter={mockOnEnter}
+            />
+        );
+
+        const inputElement = getByDisplayValue(expression);
+        expect(inputElement).toBeInTheDocument();
+    });
+
+    test('should call onExpressionChange when input value changes', () => {
+        const { getByRole } = render(
+            <InputExpression
+                expression=""
+                onExpressionChange={mockOnExpressionChange}
+                onEnter={mockOnEnter}
+            />
+        );
+
+        const inputElement = getByRole('textbox');
+        const newExpression = '5 * 5';
+        fireEvent.change(inputElement, { target: { value: newExpression } });
+
+        expect(mockOnExpressionChange).toHaveBeenCalledWith(newExpression);
+    });
+});
