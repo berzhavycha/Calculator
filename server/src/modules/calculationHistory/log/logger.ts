2dedd0eda96191530e63b16cd50de03b8c09aa1f
Andriy Berzhavych <andriyberzhavich0@gmail.com>
2024-01-04 18:12:42 +0200

    changed location of the log files in calculation

--- server/src/modules/calculationHistory/log/logger.ts
+++ server/src/modules/calculationHistory/log/logger.ts
@@ -1,3 +1,3 @@
 import { LoggerService } from "@modules/log";
 
-export const calculationLogger = new LoggerService('calculation', './src/modules/calculation/log/files/')
+export const calculationLogger = new LoggerService('calculation', './src/modules/calculationHistory/log/files/')
